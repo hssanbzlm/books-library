e8d73c7a978c4113e7e37762cd4d43d46ddb7764
Hassan Bouzlima <bouzlima.h@live.fr>
2024-03-23 10:38:38 +0100

    Update create book dto

--- src/book/dto/create-book.dto.ts
+++ src/book/dto/create-book.dto.ts
@@ -1,16 +1,27 @@
 import { Type } from 'class-transformer';
-import { IsArray, IsDate, IsNumber, IsString } from 'class-validator';
+import {
+  IsDate,
+  IsNumber,
+  IsString,
+  IsNotEmpty,
+  Min,
+  ArrayMinSize,
+} from 'class-validator';
 
 export class CreateBookDto {
+  @IsNotEmpty()
   @IsString()
   title: string;
+  @Min(50)
   @IsNumber()
   numberOfPages: number;
+  @IsNotEmpty()
   @IsString()
   edition: string;
   @Type(() => Date)
   @IsDate()
   date: Date;
-  @IsArray()
+  @IsNumber({}, { each: true })
+  @ArrayMinSize(1)
   authorIds: number[];
 }
