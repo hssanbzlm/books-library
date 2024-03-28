9d3d24cfd22db8d080c767d68d4a6c5d1af93ab2
Hassan Bouzlima <bouzlima.h@live.fr>
2024-03-28 08:28:12 +0100

    Add author controller parsing pipe

--- src/author/author.controller.ts
+++ src/author/author.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { AuthorService } from './author.service';
 import { CreateAuthorDto } from './dtos/create-author.dto';
@@ -26,13 +27,16 @@ export class AuthorController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authorService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.authorService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthorDto: UpdateAuthorDto) {
-    return this.authorService.update(+id, updateAuthorDto);
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateAuthorDto: UpdateAuthorDto,
+  ) {
+    return this.authorService.update(id, updateAuthorDto);
   }
 
   @Delete(':id')
