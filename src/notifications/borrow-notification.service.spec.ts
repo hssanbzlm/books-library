d1f69d9c0a50a0bcc213e73442113491e5ca312c
hssan bouzlima <hassanbouzlima@gmail.com>
2025-05-23 10:58:21 +0100

    update some imports path

--- src/notifications/borrow-notification.service.spec.ts
+++ src/notifications/borrow-notification.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { BorrowNotificationService } from '../borrow-notification.service';
+import { BorrowNotificationService } from './borrow-notification.service';
 
 describe('BorrowNotificationService', () => {
   let service: BorrowNotificationService;
