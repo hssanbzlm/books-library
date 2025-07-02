f3be7c620c6313671c91a248c78e645d9bf32ade
hssan bouzlima <hassanbouzlima@gmail.com>
2025-07-02 16:59:07 +0100

    Add redis env

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import Redis, { Redis as RedisClient } from 'ioredis';
 
 @Injectable()
@@ -7,10 +8,21 @@ export class RedisService implements OnModuleInit, OnModuleDestroy {
   private subscriber: RedisClient;
 
   private handlers = new Map<string, (data: any) => void>();
+  constructor(private readonly configService: ConfigService) {}
 
   onModuleInit() {
-    this.publisher = new Redis();
-    this.subscriber = new Redis();
+    const host = this.configService.get<string>('REDIS_HOST');
+    const port = this.configService.get<number>('REDIS_PORT');
+    const username = this.configService.get<string>('REDIS_USERNAME');
+    const password = this.configService.get<string>('REDIS_PASSWORD');
+
+    this.publisher = new Redis({
+      host,
+      port,
+      username,
+      password,
+    });
+    this.subscriber = new Redis({ host, port, username, password });
 
     this.subscriber.on('message', (channel, message) => {
       const handler = this.handlers.get(channel);
