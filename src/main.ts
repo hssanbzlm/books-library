3fdafb40f822e439b17a39f0d30277f103faf6f8
hssan bouzlima <hassanbouzlima@gmail.com>
2025-07-03 13:31:12 +0100

    Add port env

--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,6 @@ async function bootstrap() {
     options: { host: 'localhost', port: 3003 },
   });
   await app.startAllMicroservices();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
